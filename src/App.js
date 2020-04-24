95c50500f90bc2df520aa7bf96f218e3ddcffba3
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-24 20:48:34 +0200

    created const modifiedData

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-// Redundant way to import components:
-
-// import Cards from "./components/Cards/Cards";
-// import Chart from "./components/Chart/Chart";
-// import CountryPicker from "./components/CountryPicker/CountryPicker";
-
-// Optimal way to import components:
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api"; 
 
 class App extends React.Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,18 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-const fetchData = async () => {
+export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
+    const { data } = await axios.get(url);
+ 
+    const modifiedData = {
+      confirmed:  data.confirmed,
+      recovered:  data.recovered,
+      deaths:     data.deaths,
+      lastUpdate: data.lastUpdate
+    }
 
+    return response;
     console.log(response);
   } catch (error) {
 
