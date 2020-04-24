08f0649783e3af2a19d05d1af84887369adf139e
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-24 16:57:39 +0200

    imported components to App.js + created components index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+// Redundant way to import components:
+
+// import Cards from "./components/Cards/Cards";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+// Optimal way to import components:
+import { Cards, Chart, CountryPicker } from "./components";
+
 class App extends React.Component {
   render() {
     return (
