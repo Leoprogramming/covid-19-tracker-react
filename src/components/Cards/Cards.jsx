39ad8d7f1400bd9b3f5cf84856f2d07421e5448c
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-25 17:10:54 +0200

    created 2 more cards + fixed bugs

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, Typography, Grid, StylesProvider } from "@material-ui/core";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import styles from "./Cards.module.css";
 
@@ -15,6 +15,22 @@ const Cards = (props) => {
             <Typography variant="h5" gutterBottom>REAL DATA</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
           </CardContent>
         </Grid>
       </Grid>
