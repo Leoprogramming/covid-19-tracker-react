36ed9af6a7f0ed4eb37fdb58d294be0af9cfa90b
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-25 17:15:00 +0200

    destructured the data in const Cards - Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,16 +3,18 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
-  console.log(props);
-  
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  console.log(confirmed)
+  if(!confirmed) {
+    return "loading...";
+  }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
-            <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+            <Typography variant="h5" gutterBottom>{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
